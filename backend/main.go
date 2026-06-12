048f5a0de2b8e750e8902f911d1b8b8b8e4496ae
Skwashlepro <skwashlepro@gmail.com>
2026-06-12 15:31:33 +0200

    SQL connection

--- backend/main.go
+++ backend/main.go
@@ -3,8 +3,32 @@ package	main
 import	(
 	"net/http"
 	"fmt"
+	"database/sql"
+	"os"
+	_ "github.com/lib/pq"
 )
 func	main(){
+	connStr := "host=" + os.Getenv("DB_HOST") +
+    " port=" + os.Getenv("DB_PORT") +
+    " user=" + os.Getenv("DB_USER") +
+    " password=" + os.Getenv("DB_PASSWORD") +
+    " dbname=" + os.Getenv("DB_NAME") +
+    " sslmode=disable"
+// PostGreSQL a besoin de recevoir une string seule qui comporte tout les params comme ca :
+// Result: "host=postgres port=5432 user=myuser password=mypass dbname=mydb sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		fmt.Println("Error opening db:", err)
+		return
+	}
+
+	err = db.Ping()
+		if err != nil {
+		fmt.Println("Error connecting to db:", err)
+		return
+	}
+	fmt.Println("Connected to PostgreSQL!")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "homepage HTTP request")
 	})
